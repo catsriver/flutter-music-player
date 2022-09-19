28bed0eac9cbd65d6ec02340fd69001fb993a299
猫河 <catsriver@163.com>
2022-09-19 17:03:41 +0800

    feat: 添加登陆页面

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 import '../screens/home/home_screen.dart';
+import '../screens/login/login_screen.dart';
 
 final router = GoRouter(
   debugLogDiagnostics: true,
+  initialLocation: '/login',
   routes: [
     GoRoute(
       name: 'home',
@@ -12,6 +14,14 @@ final router = GoRouter(
       pageBuilder: (context, state) => MaterialPage(
         key: state.pageKey,
         child: const HomeScreen(),
+      ),
+    ),
+    GoRoute(
+      name: 'login',
+      path: '/login',
+      pageBuilder: (context, state) => MaterialPage(
+        key: state.pageKey,
+        child: const LoginScreen(),
       ),
     ),
   ],
