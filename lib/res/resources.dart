b674258505338b5c360a26108b7e57a0f26e6369
猫河 <catsriver@163.com>
2022-09-20 03:52:59 +0800

    feat: 添加预设字体样式

--- lib/res/resources.dart
+++ lib/res/resources.dart
@@ -1,3 +1,4 @@
 export './dimens.dart';
 export './gaps.dart';
 export './colors.dart';
+export './styles.dart';
---
+++ lib/res/styles.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import './dimens.dart';
+import './colors.dart';
+
+class TextStyles {
+  static TextStyle bigTitle = TextStyle(
+    fontSize: Dimens.fontSp26,
+    color: Colours.textNormal,
+    fontWeight: FontWeight.bold,
+  );
+
+  static TextStyle hintText = TextStyle(
+    fontSize: Dimens.fontSp20,
+    color: Colours.text_3,
+  );
+
+  static TextStyle tipText = const TextStyle(
+    color: Colours.textTips,
+  );
+}
