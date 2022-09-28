0c4f175803a4f57f07bd7ed6cabdb48976aa1251
猫河 <catsriver@163.com>
2022-09-28 15:08:23 +0800

    refactor: 使用 `HorizontalScrollView` 重构首页圆形图标入口列表

--- lib/screens/search/widgets/block_ball_menu.dart
+++ lib/screens/search/widgets/block_ball_menu.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../providers/home/ball_menu_provider.dart';
 import '../../../res/resources.dart';
+import '../../../widgets/common/horizontal_scroll_view.dart';
 import './ball_menu_item.dart';
 
 class BlockBallMenu extends ConsumerWidget {
@@ -15,20 +16,15 @@ class BlockBallMenu extends ConsumerWidget {
     return Container(
       color: Colours.secondaryBg,
       padding: EdgeInsets.symmetric(vertical: Dimens.vGapDp28),
-      child: SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: Dimens.hGapDp24 / 2),
-          child: Row(
-            children: menus
-                .map((menu) => BallMenuItem(
-                      iconUrl: menu.iconUrl,
-                      name: menu.name,
-                      press: () => print(menu.url),
-                    ))
-                .toList(),
-          ),
-        ),
+      child: HorizontalScrollView(
+        padding: Dimens.hGapDp24 / 2,
+        children: menus
+            .map((menu) => BallMenuItem(
+                  iconUrl: menu.iconUrl,
+                  name: menu.name,
+                  press: () => print(menu.url),
+                ))
+            .toList(),
       ),
     );
   }
